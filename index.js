4af9588b2f2bb8b94bfb7f5124906a192214bd61
rene-manzatto <139180722+rene-manzatto@users.noreply.github.com>
2023-08-07 20:26:35 +0000

    atualizando

---
+++ index.js
@@ -0,0 +1,9 @@
+const nomes = ['Rene', 'Mary', 'Matteo', 'Livia', 'Eliézer'];
+const idades = [33, 33, 4, 2, 1];
+
+const exibirNomesIdades = [nomes, idades];
+
+for (let indice = 0; indice < exibirNomesIdades.length; indice++) {
+    console.log(exibirNomesIdades[indice]);
+
+}
